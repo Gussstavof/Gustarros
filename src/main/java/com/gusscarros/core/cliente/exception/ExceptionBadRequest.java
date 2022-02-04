092b3c3dee76bc1b0878d936498ba2e493b5bcff
Gustavo Ferreira <g717397@gmail.com>
2022-02-04 17:56:28 -0300

    Third commit

---
+++ src/main/java/com/gusscarros/core/cliente/exception/ExceptionBadRequest.java
@@ -0,0 +1,12 @@
+package com.gusscarros.core.cliente.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ExceptionBadRequest extends RuntimeException {
+
+    public ExceptionBadRequest(String message) {
+        super(message);
+    }
+}
